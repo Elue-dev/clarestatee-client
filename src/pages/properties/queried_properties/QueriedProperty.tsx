040c48bc07be6f0a2a991550d1ac5f4f281e9bee
Wisdom <eluewisdom@gmail.com>
2023-01-03 14:02:34 +0100

    patch: modify query propwerties page

--- src/pages/properties/queried_properties/QueriedProperty.tsx
+++ src/pages/properties/queried_properties/QueriedProperty.tsx
@@ -33,10 +33,6 @@ export default function QueriedProperty() {
     setLoading(false);
   };
 
-  console.log(loading);
-
-  console.log(properties);
-
   if (!properties) {
     return <Loader />;
   }
@@ -68,78 +64,80 @@ export default function QueriedProperty() {
             </h3>
           )}
 
-          {properties?.map((property: any) => {
-            const {
-              _id,
-              name,
-              price,
-              purpose,
-              slug,
-              images,
-              availability,
-              createdAt,
-              toilets,
-              bedrooms,
-              bathrooms,
-            } = property;
-            return (
-              <Link key={_id} to={`/property/${slug}`}>
-                <div className={styles["properties__details"]}>
-                  <div className={styles["properties__details__image"]}>
-                    <img src={images[0]} alt={name} />
-                    <p
-                      className={styles["property__availability"]}
-                      style={{
-                        background:
-                          availability === "Available"
-                            ? "rgba(136, 229, 29, 0.575)"
-                            : "rgba(243, 90, 52, 0.411)",
-                      }}
-                    >
-                      {" "}
-                      {availability}
-                    </p>
-                    <p className={styles["property__purpose"]}>
-                      <span>{purpose}</span>
-                    </p>
-                    <span className={styles["camera__icon"]}>
-                      <BsCamera />
-                      <span>{images.length}</span>
-                    </span>
-                  </div>
-                  <div className={styles["properties__details__texts"]}>
-                    <p className={styles["property__name"]}>
-                      <span>{name}</span>
-                    </p>
+          <div className={styles["properties__wrap"]}>
+            {properties?.map((property: any) => {
+              const {
+                _id,
+                name,
+                price,
+                purpose,
+                slug,
+                images,
+                availability,
+                createdAt,
+                toilets,
+                bedrooms,
+                bathrooms,
+              } = property;
+              return (
+                <Link key={_id} to={`/property/${slug}`}>
+                  <div className={styles["properties__details"]}>
+                    <div className={styles["properties__details__image"]}>
+                      <img src={images[0]} alt={name} />
+                      <p
+                        className={styles["property__availability"]}
+                        style={{
+                          background:
+                            availability === "Available"
+                              ? "rgba(136, 229, 29, 0.575)"
+                              : "rgba(243, 90, 52, 0.411)",
+                        }}
+                      >
+                        {" "}
+                        {availability}
+                      </p>
+                      <p className={styles["property__purpose"]}>
+                        <span>{purpose}</span>
+                      </p>
+                      <span className={styles["camera__icon"]}>
+                        <BsCamera />
+                        <span>{images.length}</span>
+                      </span>
+                    </div>
+                    <div className={styles["properties__details__texts"]}>
+                      <p className={styles["property__name"]}>
+                        <span>{name}</span>
+                      </p>
 
-                    <p className={styles["property__id"]}>
-                      <MdDateRange />
-                      {new Date(createdAt).toDateString()}
-                    </p>
-                    <p className={styles["property__location"]}>
-                      <ImLocation2 />
-                      {location}
-                    </p>
-                    <div className={styles["interior__info"]}>
-                      <p>
-                        <FaBed /> {bedrooms}
+                      <p className={styles["property__id"]}>
+                        <MdDateRange />
+                        {new Date(createdAt).toDateString()}
                       </p>
-                      <p>
-                        <FaToilet /> {toilets}
+                      <p className={styles["property__location"]}>
+                        <ImLocation2 />
+                        {location}
                       </p>
-                      <p>
-                        <FaBath /> {bathrooms}
+                      <div className={styles["interior__info"]}>
+                        <p>
+                          <FaBed /> {bedrooms}
+                        </p>
+                        <p>
+                          <FaToilet /> {toilets}
+                        </p>
+                        <p>
+                          <FaBath /> {bathrooms}
+                        </p>
+                      </div>
+                      <p className={styles["property__price"]}>
+                        <span>NGN{new Intl.NumberFormat().format(price)}</span>
+                        /night
                       </p>
                     </div>
-                    <p className={styles["property__price"]}>
-                      <span>NGN{new Intl.NumberFormat().format(price)}</span>
-                      /night
-                    </p>
                   </div>
-                </div>
-              </Link>
-            );
-          })}
+                </Link>
+              );
+            })}
+          </div>
         </>
       )}
     </section>
